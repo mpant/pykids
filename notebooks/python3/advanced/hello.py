f385ff49bb0cc536f43d1b223f384785f87c310b
Meenal Pant <meenalpant@gmail.com>
2018-05-29 21:05:38 +0000

    Make changes to the git structure

---
+++ notebooks/python3/advanced/hello.py
@@ -0,0 +1,5 @@
+def say_hello():
+    print("Hello World again!")
+    
+if __name__ == "__main__":
+    say_hello()
---
+++ notebooks/python3/advanced/hello_again.py
@@ -0,0 +1,9 @@
+def say_hello():
+    return("Hello World!")
+
+def main():
+    print(say_hello())
+    print (5*2)
+
+if __name__ == "__main__":
+    main()
